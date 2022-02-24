4c97dc698ede1ce7fd9682161180a9dafe7527ed
Johnatan Carvalho Souza <johnatancarvalho06@gmail.com>
2022-02-24 13:27:22 -0300

    feat: implemented navigation logic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,15 +5,15 @@ import NetflixAvatar from "../../assets/img/netflix_avatar.png";
 
 const Header = ({ black }) => {
   return (
-    <header className={black && "header--black"}>
+    <header className={black ? "header--black" : ""}>
       <div className="header--logo">
         <a href="/">
-          <img src={NetflixLogo} alt="Netflix" className="" />
+          <img src={NetflixLogo} alt="Netflix" />
         </a>
       </div>
       <a href="/">
         <div className="header--user">
-          <img src={NetflixAvatar} alt="User" className="" />
+          <img src={NetflixAvatar} alt="User" />
         </div>
       </a>
     </header>
--- src/components/MovieRow/MovieRow.jsx
+++ src/components/MovieRow/MovieRow.jsx
@@ -2,20 +2,46 @@ import "./MovieRow.css";
 
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import { useState } from "react";
 
 const MovieRow = ({ title, items }) => {
+  const [scrollX, setScrollX] = useState(-400);
+
+  const handleLeftArrow = () => {
+    let x = scrollX + Math.round(window.innerWidth / 2);
+    if (x > 0) {
+      x = 0;
+    }
+    setScrollX(x);
+  };
+
+  const handleRightArrow = () => {
+    let x = scrollX - Math.round(window.innerWidth / 2);
+    let listWidth = items.results.length * 150;
+    if (window.innerWidth - listWidth > x) {
+      x = window.innerWidth - listWidth - 60;
+    }
+    setScrollX(x);
+  };
+
   return (
     <div className="movieRow">
       <h2>{title}</h2>
-      <div className="movieRow--left">
+      <div className="movieRow--left" onClick={handleLeftArrow}>
         <NavigateBeforeIcon style={{ fontSize: 50 }} />
       </div>
-      <div className="movieRow--right">
+      <div className="movieRow--right" onClick={handleRightArrow}>
         <NavigateNextIcon style={{ fontSize: 50 }} />
       </div>
 
       <div className="movieRow--listarea">
-        <div className="movieRow--list">
+        <div
+          className="movieRow--list"
+          style={{
+            marginLeft: scrollX,
+            width: items.results.length * 150,
+          }}
+        >
           {items.results.length > 0 &&
             items.results.map((item, key) => (
               <div className="movieRow--item" key={key}>
